a9d04a0b926de997b64c4ba84f85ffa95ac6a5aa
DukeBJ <bjworld@gmail.com>
2020-07-29 18:19:37 +0300

    next

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,117 @@
-const gulp = require('gulp'); // Подключаем gulp
-const browserSync = require('browser-sync').create();
-const watch = require('gulp-watch');
-const sass = require('gulp-sass');
+const gulp = require("gulp"); // Подключаем Gulp
+const browserSync = require("browser-sync").create();
+const watch = require("gulp-watch");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+const notify = require("gulp-notify");
+const plumber = require("gulp-plumber");
+const pug = require("gulp-pug");
+const del = require("del");
 
-gulp.task('sass', function(callback) {
-    return gulp.src('./src/sass/**/*.scss')
-        .pipe(sass())
-        .pipe( gulp.dest('./app/css/') );
+// Таск для сборки Gulp файлов
+gulp.task('pug', function(callback) {
+    return gulp.src('./src/pug/**/*.pug')
+        // Следим за ошибками
+        .pipe( plumber({
+            errorHandler: notify.onError(function(err){
+                return {
+                    title: 'PUG error',
+                    sound: false,
+                    message: err.message
+                }
+            })
+        }))
+        .pipe( pug({
+            pretty: true
+        }) )
+        .pipe( gulp.dest('./build/') )
+        // следим за изменениями
+        .pipe( browserSync.stream() );
     callback();
 });
 
-// Слежение за изменением в файлах
-gulp.task('watch', function() {
-    watch(['./app/*.html', './app/css/**/*.css'], gulp.parallel( browserSync.reload ))
+// Таск для компиляции SCSS в CSS
+gulp.task("scss", function(callback) {
+    return gulp
+        .src("./src/scss/style.scss")
+        .pipe(
+            plumber({
+                errorHandler: notify.onError(function(err) {
+                    return {
+                        title: "Styles",
+                        sound: false,
+                        message: err.message
+                    };
+                })
+            })
+        )
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(
+            autoprefixer({
+                overrideBrowserslist: ["last 4 versions"]
+            })
+        )
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("./build/css/"))
+        .pipe(browserSync.stream());
+    callback();
 });
 
-// Задача для старта сервера
-gulp.task('server', function() {
-   browserSync.init({
-       server: {
-           baseDir: "./app/"
-       }
-   })
+// Копирование Изображений
+gulp.task("copy:img", function(callback) {
+    return gulp.src("./src/img/**/*.*").pipe(gulp.dest("./build/img/"));
+    callback();
 });
 
-gulp.task('default', gulp.parallel('server', 'watch'));
+// Копирование Скриптов
+gulp.task("copy:js", function(callback) {
+    return gulp.src("./src/js/**/*.*").pipe(gulp.dest("./build/js/"));
+    callback();
+});
+
+// Слежение за HTML и CSS и обновление браузера
+gulp.task("watch", function() {
+    // Следим за картинками и скриптами и обновляем браузер
+    watch(
+        ["./build/js/**/*.*", "./build/img/**/*.*"],
+        gulp.parallel(browserSync.reload)
+    );
+
+    // Запуск слежения и компиляции SCSS с задержкой
+    watch("./src/scss/**/*.scss", function() {
+        setTimeout(gulp.parallel("scss"), 500);
+    });
+
+    // Слежение за PUG и сборка
+    watch(["./src/pug/**/*.pug", "./src/data/**/*.json"], gulp.parallel("pug"));
+
+    // Следим за картинками и скриптами, и копируем их в build
+    watch("./src/img/**/*.*", gulp.parallel("copy:img"));
+    watch("./src/js/**/*.*", gulp.parallel("copy:js"));
+});
+
+// Задача для старта сервера из папки app
+gulp.task("server", function() {
+    browserSync.init({
+        server: {
+            baseDir: "./build/"
+        }
+    });
+});
+
+gulp.task("clean:build", function() {
+    return del("./build");
+});
+
+// Дефолтный таск (задача по умолчанию)
+// Запускаем одновременно задачи server и watch
+gulp.task(
+    "default",
+    gulp.series(
+        gulp.parallel("clean:build"),
+        gulp.parallel("scss", "pug", "copy:img", "copy:js"),
+        gulp.parallel("server", "watch")
+    )
+);
---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  })
+
+console.log('%c Скрипя' + '%c ❤' + '%c и стирая пальцы в кровь | ' + '%cМаксимка 😁 ', 'background:#111; color:#fff; border-radius:7px 0 0 7px;', 'background:#111; color:#f00;', 'background:#111; color:#fff;', 'background:#111; color:#7ef; border-radius:0 7px 7px 0;');
